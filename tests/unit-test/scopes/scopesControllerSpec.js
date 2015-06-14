bdd3b639473db1f722886a7152a4f37c06d3ddc7
zack <zackshen0526@gmail.com>
2015-06-14 11:09:33 +0800

    testcase: event emit

--- tests/unit-test/scopes/scopesControllerSpec.js
+++ tests/unit-test/scopes/scopesControllerSpec.js
@@ -1,10 +1,35 @@
 describe('ScopesController', function(){
 
-    beforeEach(module('eventDemo'));
+    var parentScope,
+        parentCtrl,
+        childScope,
+        childCtrl;
 
-    it('first init scope, count should be 0', inject(function($rootScope, $controller) {
-        var scope = $rootScope.$new(),
-        ctrl = $controller('ParentController', {$scope:scope});
-        expect(scope.count).toBe(0);
-    }));
+    beforeEach(function() {
+        module('eventDemo');
+
+        inject(function($rootScope, $controller) {
+            parentScope = $rootScope.$new();
+            parentCtrl = $controller("ParentController", {$scope: parentScope});
+
+            childScope = parentScope.$new();
+            childCtrl = $controller("ChildController1", {$scope: childScope});
+        });
+    });
+
+    it('first init scope, count should be 0',function() {
+        expect(parentScope.count).toBe(0);
+        expect(childScope.count).toBe(0);
+    });
+
+
+    it('emit event, parent should received , count should be 1', function() {
+        childScope.emitEvent('ChildEvent');
+        expect(parentScope.count).toBe(1)
+        expect(childScope.count).toBe(0);
+        for (var i=0; i < 10; i++) {
+            childScope.emitEvent('ChildEvent');
+        }
+        expect(parentScope.count).toBe(11);
+    });
 });
