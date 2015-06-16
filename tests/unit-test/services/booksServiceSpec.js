17e258423b27e35eca10ad5ba89b8e4a7ad306d0
zack <zackshen0526@gmail.com>
2015-06-16 22:10:33 +0800

    fix testcase, mock http return value

--- tests/unit-test/services/booksServiceSpec.js
+++ tests/unit-test/services/booksServiceSpec.js
@@ -10,6 +10,12 @@ describe('BooksService', function(){
             ctrl = _$controller_('customServiceController', {
                 $scope: $scope
             });
+            $httpBackend.whenGET('/fixtures/books.json')
+            .respond([
+                {'name':'book1', 'price': 10},
+                {'name':'book2', 'price': 30},
+                {'name':'book3', 'price': 50}
+            ]);
         });
     });
 
@@ -19,27 +25,12 @@ describe('BooksService', function(){
     });
 
     it('list all books , count should be 3',function() {
-
-        $httpBackend.whenGET('/fixtures/books.json')
-        .respond([
-            {'name':'book1', 'price': 10},
-            {'name':'book2', 'price': 30},
-            {'name':'book3', 'price': 50}
-        ]);
         $scope.getAllBooks();
         $httpBackend.flush();
-
         expect($scope.books.length).toBe(3);
     });
 
     it('filter books , price is greater than 20 should be 2',function() {
-
-        $httpBackend.whenGET('/fixtures/books.json')
-        .respond([
-            {'name':'book1', 'price': 10},
-            {'name':'book2', 'price': 30},
-            {'name':'book3', 'price': 50}
-        ]);
         $scope.filterPrice = 20;
         $scope.filterBooks();
         $httpBackend.flush();
