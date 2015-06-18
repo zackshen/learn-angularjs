8418eab32d8a577ebd4aeb5f72cc034e24712733
zack <zackshen0526@gmail.com>
2015-06-18 22:13:42 +0800

    load template

--- directives/directive.js
+++ directives/directive.js
@@ -1,5 +1,7 @@
 var app = angular.module('myApp', []);
-app.directive('date', ['$log', '$interval',function($log, $interval) {
+// custom directives
+// clock
+app.directive('clock', ['$log', '$interval',function($log, $interval) {
     return {
         scope: {},
         restrict: 'E',
@@ -7,6 +9,7 @@ app.directive('date', ['$log', '$interval',function($log, $interval) {
         template: '<h1>{{tsStr}}</h1>',
         link: function(scope, element, attr) {
             if (attr.refresh != "") {
+                scope.tsStr = moment().format('YYYY-MM-DD HH:mm:ss');
                 $interval(function() {
                     scope.tsStr = moment().format('YYYY-MM-DD HH:mm:ss');
                 }, parseInt(attr.refresh)*1000);
@@ -14,3 +17,10 @@ app.directive('date', ['$log', '$interval',function($log, $interval) {
         }
     }
 }]);
+
+// laad template from server
+app.directive('complexHugeDirective', function() {
+    return {
+        templateUrl: "./huge.tpl"
+    }
+});
