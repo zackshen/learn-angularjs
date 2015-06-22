c254bf0411226c5992b03688c9881464875ca4ba
zack <zackshen0526@gmail.com>
2015-06-22 19:49:25 +0800

    complete paginator testcase

--- directives/paginator.html.tpl
+++ directives/paginator.html.tpl
@@ -5,7 +5,9 @@
                 <span aria-hidden="true">&laquo;</span>
             </a>
         </li>
-        <li ng-repeat="page in pages" ng-click="gotoPage(page)" ng-class="{active: isCurrentPage(page)}"><a href="javascript:;">{{page}}</a></li>
+        <li ng-repeat="page in pages" ng-class="{active: isCurrentPage(page)}">
+            <a href="javascript:;" ng-click="gotoPage(page)" >{{page}}</a>
+        </li>
         <li ng-class="{disabled: noNext()}">
             <a href="javascript:;" aria-label="Next" ng-click="nextPage()">
                 <span aria-hidden="true">&raquo;</span>
--- tests/unit-test/directives/directiveSpec.js
+++ tests/unit-test/directives/directiveSpec.js
@@ -3,13 +3,13 @@ describe("directive: paginator", function() {
     var $scope, element, lis;
 
     beforeEach(module('directiveApp'));
-    //beforeEach(module('directives/paginator.tpl'));
     beforeEach(module('templates'));
     beforeEach(inject(function($compile, $rootScope) {
+        element = angular.element('<paginator page-count="pageCount" current-page="currentPage"></paginator>');
         $scope = $rootScope;
         $scope.pageCount = 5;
         $scope.currentPage = 1;
-        element = $compile('<paginator page-count="pageCount" current-page="currentPage"></paginator>')($scope);
+        $compile(element)($scope);
         $scope.$digest();
         lis = function() {
             return element.find('li');
@@ -33,19 +33,22 @@ describe("directive: paginator", function() {
         expect(lis().eq(1).hasClass('active')).toBe(true);
     });
 
-    it("click page2, currentpage should be 2", function() {
-        // TODO
-        // var page2 = lis().eq(2).find('a').eq(0);
-        // $scope.$digest();
-        // page2.click();
-        // expect($scope.currentPage).toBe(2);
+    it("click page4, currentpage should be 4", function() {
+        var page4 = lis().eq(4).find('a').eq(0);
+        page4.triggerHandler('click');
+        expect($scope.currentPage).toBe(4);
     });
 
-    it("change pageCount, paginator buttons count should be 8", function() {
-        $scope.pageCount = 6;
+    it("click page5, nextPage should be disabled", function() {
+        var page5 = lis().eq(5).find('a').eq(0);
+        page5.triggerHandler('click');
+        expect(lis().eq(6).hasClass('disabled')).toBe(true);
+    });
+
+    it("change pageCount, paginator buttons count should be 10", function() {
+        $scope.pageCount = 8;
         $scope.$digest();
-        expect(lis().length).toBe(8);
+        expect(lis().length).toBe(10);
     });
-
 
 });
